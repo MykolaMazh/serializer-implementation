cbbd9ff9a8e50307cb51665f19574368248b5dcb
MykolaMazhula <mykm3ua@gmail.com>
2024-04-28 19:28:27 +0200

    Create CarSerializer

--- car/serializers.py
+++ car/serializers.py
@@ -1,5 +1,24 @@
 from rest_framework import serializers
 
+from car.models import Car
+
 
 class CarSerializer(serializers.Serializer):
-    pass
+    id = serializers.IntegerField(read_only=True)
+    manufacturer = serializers.CharField(max_length=64)
+    model = serializers.CharField(max_length=64)
+    horse_powers = serializers.IntegerField()
+    is_broken = serializers.BooleanField()
+    problem_description = serializers.CharField(required=False, allow_null=True)
+
+    def validate_horse_powers(self, value):
+        if not 1<= value <= 1914:
+            raise serializers.ValidationError("Horse powers should be in 1-1914 gap.")
+        else:
+            return value
+
+    def create(self, validated_data):
+        return Car.objects.create(**validated_data)
+
+
+
